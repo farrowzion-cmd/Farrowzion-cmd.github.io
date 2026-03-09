6a03bfa40037b645aad860ceb5189ffe151b794a
Redacted <farrowzion@gmail.com>
2026-03-09 14:06:07 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hacking Simulation</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="terminal" id="terminal-output">
+        <!-- Output lines will be added here by JavaScript -->
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
